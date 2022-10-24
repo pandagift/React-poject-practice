54682bd81280ec7f2ee28e7f3da460a538f14b4c
Arapov Vasyl <panda.dagon@gmail.com>
2022-10-24 18:57:40 +0300

    modify

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,13 @@
+import './search-panel.css';
+
+const SearchPanel = () => {
+    return (
+        <input
+            type='text'
+            className="form-control search-input"
+            placeholder="Пошук працівника"
+        ></input>
+    )
+}
+
+export default SearchPanel;
